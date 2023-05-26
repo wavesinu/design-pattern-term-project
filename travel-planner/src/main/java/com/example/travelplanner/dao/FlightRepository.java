eedec705c7c38f3224bda226488bb703a30ec77d
wavesinu <wavesinu@gmail.com>
2023-05-26 15:43:26 +0900

    Delete FlightRepository.java

--- travel-planner/src/main/java/com/example/travelplanner/dao/FlightRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.travelplanner.dao;
-
-import com.example.travelplanner.model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-
-}
